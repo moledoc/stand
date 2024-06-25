7114b04efad021e7ada68255b509918f460f72e7
Meelis Utt <meelis.utt@gmail.com>
2024-06-25 22:22:11 +0200

    minor tweaks and ccls formatting

--- stand.c
+++ stand.c
@@ -1,45 +1,47 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <pthread.h>
 
 #include "raylib.h"
 
-int cycle = 60*60; // minutes
-int stand = 10*60; // minutes
+#define CYCLE 60 * 60 // seconds
+#define STAND 10 * 60 // seconds
+
 int ticker = 0;
 int end_ticking = 0;
 
 void *ticking(void *vargp) {
-	while(!end_ticking) {
-		sleep(1);
-		ticker += 1;
-	}
-	return NULL;
+  while (!end_ticking) {
+    sleep(1);
+    ticker += 1;
+  }
+  return NULL;
 }
 
 int should_stand() {
-	int tick = ticker%(cycle);
-	return 0 <= tick && tick < stand;
+  int tick = ticker % (CYCLE);
+  return 0 <= tick && tick < STAND;
 }
 
 int main(void) {
-	pthread_t thread_id;
-	pthread_create(&thread_id, NULL, ticking, NULL);
+  pthread_t thread_id;
+  pthread_create(&thread_id, NULL, ticking, NULL);
 
-	while (1) {
-		if (should_stand()) {
-			InitWindow(30, 30, "stand");
-			while (!WindowShouldClose() && should_stand()) {
-				BeginDrawing();
-				ClearBackground(RED);
-				SetWindowPosition(0, 0);
-				EndDrawing();
-				sleep(1); // NOTE: to slow down the loop a bit; for faster loop change the sleep to <1s
-			}
-			CloseWindow();
-		}
-		sleep(60);
-	}
-	pthread_join(thread_id, NULL);
-	return 0;
+  while (1) {
+    if (should_stand()) {
+      InitWindow(30, 30, "stand");
+      SetTargetFPS(5);
+      while (!WindowShouldClose() && should_stand()) {
+        BeginDrawing();
+        SetWindowState(FLAG_WINDOW_TOPMOST);
+        ClearBackground(RED);
+        SetWindowPosition(0, 0);
+        EndDrawing();
+      }
+      CloseWindow();
+    }
+    sleep(60);
+  }
+  pthread_join(thread_id, NULL);
+  return 0;
 }
